ea2602ce2391263d71d60a0db10da9b1bcbeb29f
itskrixhna32 <itskrixhna32@gmail.com>
2025-09-12 16:14:29 +0000

    HCF of 2 numbers

---
+++ HCF.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,gcd;
+    printf("enter first number: ");
+    scanf("%d",&a);
+    printf("enter second number: ");
+    scanf("%d",&b);
+    for(int i=1;i<=a && i<=b;++i)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            gcd=i;
+        }
+    }
+    printf("GCD of %d and %d is: %d\n",a,b,gcd);
+    return 0;
+}
