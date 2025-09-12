90918361bd9517520212e03c276336cbd47437d3
itskrixhna32 <itskrixhna32@gmail.com>
2025-09-12 11:51:35 +0000

    month and its days

---
+++ month_day.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    int month,days;
+    printf("Enter month numnber: ");
+    scanf("%d",&month);
+    switch(month)
+    {
+        case 1: printf("january has 31 days\n");
+                break;
+        case 2: printf("february has 28 days\n");
+                break;
+        case 3: printf("March has 31 days\n");
+                break;
+        case 4: printf("April has 30 days\n");
+                break;
+        case 5: printf("May has 31 days\n");
+                break;
+        case 6: printf("June has 30 days\n");
+                break;
+        case 7: printf("July has 31 days\n");
+                break;
+        case 8: printf("August has 31 days\n");
+                break;
+        case 9: printf("September has 30 days\n");
+                break;
+        case 10: printf("October has 31 days\n");
+                break;
+        case 11: printf("November has 30 days\n");
+                break;
+        case 12: printf("December has 31 days\n");
+                break;
+        default : printf("Invalid month numbner\n");
+
+    }
+    return 0;
+}
