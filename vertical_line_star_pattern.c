22a676c40ce77b7ac9f8b1fe173a08a1710c08b5
itskrixhna32 <itskrixhna32@gmail.com>
2025-09-14 19:03:12 +0000

    vertical star line pattern

---
+++ vertical_line_star_pattern.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<=1;i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    for(int i=1;i<=3;i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    for(int i=1;i<=5;i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    for(int i=1;i<=3;i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    for(int i=1;i<=1;i++)
+    {
+        printf("*\n");
+    }
+    return 0;
+}
