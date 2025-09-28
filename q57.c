2ad41b81a6065028a562e9ad7a29c86777b4ed36
itskrixhna32 <itskrixhna32@gmail.com>
2025-09-28 11:55:39 +0000

    sum of an array

---
+++ q57.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5];
+    for(int i=0;i<=5;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<=5;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    int sum = 0;
+    for(int i=0;i<=5;i++)
+    {
+        sum = sum + arr[i];
+    }
+    printf("\nSum = %d ",sum);
+    
+    return 0;
+}
