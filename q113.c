500fbf7f81d31e07d9b9dc70f7a2f25e5ac81fe2
itskrixhna32 <itskrixhna32@gmail.com>
2025-11-25 16:12:06 +0000

    Write a program to take an integer array arr and an integer k as inputs. The task is to find the kth smallest element in the array. Print the kth smallest element as output.

---
+++ q113.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &k);
+
+    // Sort array using simple bubble sort
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("%d\n", arr[k - 1]);
+    return 0;
+}
